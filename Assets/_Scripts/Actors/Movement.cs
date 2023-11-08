f052e08dfde41eeeeeefa54e0e4d092eca4844a6
Aldo Zulhaq <imahumanty@gmail.com>
2023-11-08 21:20:55 +0700

    Player Fishlike Movement (Base)

--- Assets/_Scripts/Actors/Movement.cs
+++ Assets/_Scripts/Actors/Movement.cs
@@ -4,24 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 5f;
-    
-    private Rigidbody rigidBody;
-    Vector3 movement;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidBody = GetComponent<Rigidbody>();
-    }
+    [SerializeField] float maxMoveSpeed = 5f;
+    [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] float acceleration = 10f;
+    [SerializeField] float deceleration = 20f;
+
+    private Vector3 previousDir = Vector3.up;
+    private float currentMoveSpeed = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.z = Input.GetAxisRaw("Vertical");
-    }
-    private void FixedUpdate()
-    {
-        rigidBody.MovePosition(rigidBody.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        float horizontalMov = Input.GetAxisRaw("Horizontal");
+        float verticalMov = Input.GetAxisRaw("Vertical");
+        
+        Vector3 moveDir = new Vector3(horizontalMov, 0f, verticalMov).normalized;
+
+        if(moveDir != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            //Acceleration
+            currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, maxMoveSpeed, acceleration * Time.deltaTime);
+
+            transform.Translate(moveDir * currentMoveSpeed * Time.deltaTime, Space.World);
+
+            previousDir = moveDir;
+        }
+        else
+        {
+            //Deceleration
+            currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, 0f, deceleration * Time.deltaTime);
+            transform.Translate(previousDir * currentMoveSpeed * Time.deltaTime, Space.World);
+        }
     }
 }
