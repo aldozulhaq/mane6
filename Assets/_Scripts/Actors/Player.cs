b7febd7fd716aafd5ac175f2949e93174ca540fe
Aldo Zulhaq <imahumanty@gmail.com>
2023-11-10 01:50:42 +0700

    Cinemachine Confinement + Land Proc (Attempt)

--- Assets/_Scripts/Actors/Player.cs
+++ Assets/_Scripts/Actors/Player.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Base Stat")]
-    [SerializeField] float maxHealth;
-    [SerializeField] float moveSpeedPercentage = 100f;
-    [SerializeField] float damagePercentage = 100f;
-    [SerializeField] float attackSpeedPercentage = 100f;
-    [SerializeField] int fireRate = 1;
-    [SerializeField] float critChancePercentage = 0f;
-    [SerializeField] float critDamagePercentage = 100f;
-    [SerializeField] float lifeStealPercentage = 0f;
-    [SerializeField] float damageReducePercentage = 0f;
-
-
     [Header("Events")]
     [SerializeField] FloatEventChannel playerHealthChannel;
 
@@ -32,7 +20,7 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        health = maxHealth;
+        health = PlayerStats.maxHealth;
     }
 
     public void TakeDamage(float damage)
---
+++ Assets/_Scripts/Datas/PlayerStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public static class PlayerStats
+{
+    public static float maxHealth = 100f;
+    public static float moveSpeedPercentage = 100f;
+    public static float damagePercentage = 100f;
+    public static float attackSpeedPercentage = 100f;
+    public static int fireRate = 1;
+    public static float critChancePercentage = 0f;
+    public static float critDamagePercentage = 100f;
+    public static float lifeStealPercentage = 0f;
+    public static float damageReducePercentage = 0f;
+}
