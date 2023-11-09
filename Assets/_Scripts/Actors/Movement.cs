28b489abd5bd6da390501490be408fe23bffb067
Aldo Zulhaq <imahumanty@gmail.com>
2023-11-09 18:11:41 +0700

    adjust extension methods to unity + refactor movement.cs

--- Assets/_Scripts/Actors/Movement.cs
+++ Assets/_Scripts/Actors/Movement.cs
@@ -16,33 +16,52 @@ public class Movement : MonoBehaviour
     Vector3 previousDir = Vector3.up;
     float currentMoveSpeed = 0f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        HandleMovementInput();
+        SendSpeedToAnimator();
+    }
+
+    private void HandleMovementInput()
     {
         float horizontalMov = Input.GetAxisRaw("Horizontal");
         float verticalMov = Input.GetAxisRaw("Vertical");
-        
+
         Vector3 moveDir = new Vector3(horizontalMov, 0f, verticalMov).normalized;
 
-        if(moveDir != Vector3.zero)
+        if (moveDir != Vector3.zero)
         {
-            float targetAngle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg;
-            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-            //Acceleration
-            currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, maxMoveSpeed, acceleration * Time.deltaTime);
-            transform.Translate(moveDir * currentMoveSpeed * Time.deltaTime, Space.World);
-
-            previousDir = moveDir;
+            RotateCharacter(moveDir);
+            ApplyAcceleration(moveDir);
         }
         else
         {
-            //Deceleration
-            currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, 0f, deceleration * Time.deltaTime);
-            transform.Translate(previousDir * currentMoveSpeed * Time.deltaTime, Space.World);
+            ApplyDeceleration();
         }
-        //send speedto animator
+    }
+
+    private void RotateCharacter(Vector3 moveDir)
+    {
+        float targetAngle = Mathf.Atan2(moveDir.x, moveDir.z) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void ApplyAcceleration(Vector3 moveDir)
+    {
+        currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, maxMoveSpeed, acceleration * Time.deltaTime);
+        transform.Translate(moveDir * currentMoveSpeed * Time.deltaTime, Space.World);
+        previousDir = moveDir;
+    }
+
+    private void ApplyDeceleration()
+    {
+        currentMoveSpeed = Mathf.MoveTowards(currentMoveSpeed, 0f, deceleration * Time.deltaTime);
+        transform.Translate(previousDir * currentMoveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void SendSpeedToAnimator()
+    {
         playerSpeedChannel.Invoke(currentMoveSpeed);
     }
 }
--- Assets/_Scripts/Helpers/ExtensionMethods.cs
+++ Assets/_Scripts/Helpers/ExtensionMethods.cs
@@ -1,4 +1,4 @@
-using System.Numerics;
+using UnityEngine;
 
 public static class ExtensionMethods
 {
@@ -9,16 +9,16 @@ public static class ExtensionMethods
 
     public static Vector3 WithX(this Vector3 vector, float x)
     {
-        return new Vector3(x, vector.Y, vector.Z);
+        return new Vector3(x, vector.y, vector.z);
     }
 
     public static Vector3 WithY(this Vector3 vector, float y)
     {
-        return new Vector3(vector.X, y, vector.Z);
+        return new Vector3(vector.x, y, vector.z);
     }
 
     public static Vector3 WithZ(this Vector3 vector, float z)
     {
-        return new Vector3(vector.X, vector.Y, z);
+        return new Vector3(vector.x, vector.y, z);
     }
 }
